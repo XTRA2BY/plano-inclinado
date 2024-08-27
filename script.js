ff2e655fba1de1ea0f847eddba573f9bb515cfcb
Thiago Machado <kerzaam@gmail.com>
2024-08-27 07:02:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,16 +8,26 @@ function calcular() {
     const Px = Math.round(P * Math.sin(theta_rad) * 100) / 100;
     const Py = Math.round(P * Math.cos(theta_rad) * 100) / 100;
     const Fr = Math.round(Py * mu * 100) / 100;
-    let resultado = `<p>el peso del cuerpo es ${P} N</p>`;
-    resultado += `<p>las componentes del peso son: Px = ${Px} N y Py = ${Py} N</p>`;
-    resultado += `<p>la fuerza Normal tiene un valor de ${Py} N</p>`;   
+    
+    let resultado = `
+        <p>el peso del cuerpo es de <strong>${P} N</strong></p>
+        <p>las componentes del peso son de: <strong>Px = ${Px} N y Py = ${Py} N</strong></p>
+        <p>la fuerza normal tiene un valor de <strong> ${Py} N</strong></p>
+    `;   
+    
     if (Fr >= Px) {
-        resultado += `<p>la fuerza de rozamiento tiene un valor de ${Px} N</p>`;
-        resultado += `<p>el cuerpo está en equilibrio</p>`;
+        resultado += `
+            <p>la fuerza de rozamiento tiene un valor de <strong> ${Px} N</strong></p>
+            <p>ll cuerpo está en equilibrio</p>
+        `;
     } else {
-        resultado += `<p>la fuerza de Rozamiento tiene un valor de ${Fr} N</p>`;
+        resultado += `
+            <p>la fuerza de rozamiento tiene un valor de <strong> ${Fr} N</strong></p>
+        `;
         const a = Math.round((Px - Fr) / masa * 100) / 100;
-        resultado += `<p>el cuerpo acelera a razón de ${a} m/s²</p>`;
+        resultado += `
+            <p>el cuerpo acelera a razón de <strong>${a} m/s²</strong></p>
+        `;
     }
 
     document.getElementById('resultados').innerHTML = resultado;
